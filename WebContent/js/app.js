d77c2319fc671584d8ef7f1c9bef3f3b99599e29
mangaldev <dev.itbhu@gmail.com>
2014-06-22 23:53:10 +0530

    .

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,6 +1,3 @@
 //mean is the module name we are using as ng-app in our main page
 
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'elasticsearch','mean.system', 'mean.search']);
-
-angular.module('mean.system', []);
-angular.module('mean.search', []);
+angular.module('iquiz', []);
--- WebContent/js/config.js
+++ WebContent/js/config.js
@@ -1,32 +1,10 @@
 //Setting up route
-angular.module('mean')
+angular.module('iquiz')
 
-.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-        when('/', {
-            templateUrl: 'views/index.html'
-        }).
-        when('/search/:query', {
-            templateUrl: 'views/search.html'
-        }).
-        when('/movie/:movieId', {
-            templateUrl: 'views/movie.html'
-        }).
-        when('/people/:peopleId', {
-            templateUrl: 'views/people.html'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
-    }
-    ]);
-
-//Whenver there will be ! in URL ,,, this file will be in role else route.js of express will be in role
-//Setting HTML5 Location Mode
-angular.module('mean')
-.config(['$locationProvider',
-    function($locationProvider) {
-        $locationProvider.hashPrefix("!");
-    }
-    ]);
+.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'index.html'
+	}).otherwise({
+		redirectTo : '/'
+	});
+} ]);
