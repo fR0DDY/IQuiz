db3ac5832221e0300f8ad58a239540da6005e530
Gaurav Kumar <gaurav.bond.itbhu@gmail.com>
2014-06-23 23:37:38 +0530

    Socket End Point

---
+++ src/com/iquiz/socket/server/SocketEndpoint.java
@@ -0,0 +1,22 @@
+package com.iquiz.socket.server;
+
+import java.util.logging.Logger;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/iquiz")
+public class SocketEndpoint {
+	private final Logger log = Logger.getLogger(getClass().getName());
+ 
+	@OnOpen
+	public void open(final Session session) {
+	}
+ 
+	@OnMessage
+	public void onMessage(final Session session, final String chatMessage) {
+		System.out.println("chatMessage: " + chatMessage);
+	}
+}
